af8ab72e050f078956658260605b574caa8bfe0d
Richard <ricky.cary1@gmail.com>
2023-01-11 16:17:22 -0600

    Frontend Complete

---
+++ src/pages/Projects.js
@@ -0,0 +1,22 @@
+import { useLoaderData } from "react-router-dom";
+
+function Projects(props) {
+
+    const projects = useLoaderData()
+
+
+    return projects.map((project) => (
+        <div>
+            <h1>{project.name}</h1>
+            <img src={project.image}/>
+            <a href={project.git}>
+                <button>Github</button>
+            </a>
+            <a href={project.live}>
+                <button>Live Site</button>
+            </a>
+        </div>
+    ))
+}
+
+export default Projects; 
